17f27ef188e587a20fcda66207b16c62767ea164
Alex Junior <alexjvm90@gmail.com>
2021-11-08 21:42:24 -0300

    feat: multer config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 const { categoriesRoutes } = require('./routes/categories');
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(categoriesRoutes);
 
 module.exports = { app };
