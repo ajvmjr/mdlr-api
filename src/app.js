d54e59b139792d77b398c8623d42a7dbcccb4589
Alex Junior <alexjvm90@gmail.com>
2021-11-30 23:43:14 -0300

    feat: adding cors package and changing app port

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 
 import path from 'path';
+
+import cors from 'cors';
+
 import categoriesRoutes from './routes/categories';
 import productsRoutes from './routes/products';
 import authRoutes from './routes/auth';
@@ -10,6 +13,9 @@ import ordersRoutes from './routes/orders';
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(categoriesRoutes);
 app.use(authRoutes);
@@ -17,6 +23,6 @@ app.use(userRoutes);
 app.use(productsRoutes);
 app.use(ordersRoutes);
 
-app.listen(3000, () => console.log('Server on at 3000'));
+app.listen(3001, () => console.log('Server on at 3001'));
 
 export default app;
